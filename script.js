578901dd3e34f45308371b97327d864fbb459b39
joselevelsup <joselevelsup@gmail.com>
2024-12-11 16:39:09 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const calc = document.getElementById("calc-form");
+
+calc.addEventListener("submit", function(e){
+  e.preventDefault();
+  //Add Code here to grab the first number input
+  const firstNum;
+  //Add Code here to grab the second number input
+  const secondNum;
+  //Add Code here to get the select element
+  const operator;
+  //Add Code here to get the result span element
+  const result;
+
+  if(operator == "add-actual-operator-value-here"){
+    //Add code here to do addition and display it.
+  }
+
+  if(operator == "add-actual-operator-value-here"){
+    //Add code here to do subtraction and display it.
+  }
+})
